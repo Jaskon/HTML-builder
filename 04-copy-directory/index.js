9716b664e531b2f70974082c03cbfd3d5f5f5b12
Eugene Orlov <jaskon97@gmail.com>
2021-11-05 23:13:47 +0300

    feat: task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,43 @@
 const path = require('path');
 const fs = require('fs/promises');
 
+function copyEntitiesRecursively(from, to) {
+  // Read all entities to copy
+  fs.readdir(from, { withFileTypes: true }).then(entities => {
+    for (const one of entities) {
+      const fromPath = path.resolve(from, one.name);
+      const toPath = path.resolve(to, one.name);
+
+      // Copy files
+      if (one.isFile()) {
+        fs.copyFile(fromPath, toPath).then(() => {
+          console.log(`Copied '${one.name}' -> '${toPath}'`);
+        });
+      }
+      // Copy directories recursively
+      if (one.isDirectory()) {
+        fs.mkdir(toPath, { recursive: true }).then(() => {
+          copyEntitiesRecursively(fromPath, toPath);
+        });
+      }
+    }
+
+    // Remove unwanted files (in case folder existed before)
+    fs.readdir(to, { withFileTypes: true }).then(newDirEntities => {
+      for (const one of newDirEntities) {
+        if (one.isFile() && !entities.map(ent => ent.name).includes(one.name)) {
+          fs.rm(path.resolve(to, one.name)).then(() => {
+            console.log(`Deleted '${one.name}' <- '${from}'`);
+          });
+        }
+      }
+    });
+  });
+}
+
 const dirPath = path.resolve('./04-copy-directory/files');
 const dirPathNew = path.resolve('./04-copy-directory/files-copy');
 
 fs.mkdir(dirPathNew, { recursive: true }).then(() => {
-  // Read all entities to copy
-  fs.readdir(dirPath, { withFileTypes: true }).then(entities => {
-    for (const one of entities) {
-      if (one.isFile()) {
-        fs.stat(path.resolve(dirPath, one.name)).then(oneStat => {
-          const parsed = path.parse(one.name);
-          // There should be used 'path.extname(one.name)' according to the task, but I think I found a better solution
-          console.log(`${parsed.name} - ${parsed.ext} - ${oneStat.size / 1000}kb`);
-        });
-      }
-    }
-  });
+  copyEntitiesRecursively(dirPath, dirPathNew);
 });
